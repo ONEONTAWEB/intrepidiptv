a8f6e3c10dd419dcf37146f2f6827cc1cade364a
ONEONTAWEB <michael@oneontawebdesign.com>
2017-05-23 23:03:07 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
+     define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '123pass');
    define('DB_DATABASE', 'stalker_db');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   define('DB_TABLE', 'administrators');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_TABLE);
 ?>
