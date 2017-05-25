8368da7570354fa9734c5d86cccc8b16591687c4
ONEONTAWEB <michael@oneontawebdesign.com>
2017-05-24 23:46:11 -0400

    Update and rename channels.txt to channels.php

---
+++ channels.php
@@ -0,0 +1,21 @@
+<name>Pac-12 National HD</name><url>http://xrxs.net/video/live-p12netw-4728.m3u8</url>
+<name>TBN HD</name><url>http://acaooyalahd2-lh.akamaihd.net/i/TBN01_delivery@186239/master.m3u8</url>
+<name>C-SPAN HD</name><url>http://cspan1-lh.akamaihd.net/i/cspan1_1@304727/index_1000_av-p.m3u8?sd=10&rebase=on</url>
+<name>CNN HD</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2720.m3u8</url>
+<name>ESPN HD</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2728.m3u8</url>
+<name>A&E HD</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2707.m3u8</url>
+<name>Cartoon Network HD</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/544.m3u8</url>
+<name>ABC</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2706.m3u8</url>
+<name>Animal Planet</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2693.m3u8</url>
+<name>Discovery Channel</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2687.m3u8</url>
+<name>Fox</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2698.m3u8</url>
+<name>History</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2682.m3u8</url>
+<name>LifeTime</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2708.m3u8</url>
+<name>National Geographic</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2680.m3u8</url>
+<name>TBS</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2665.m3u8</url>
+<name>Cinemax</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2689.m3u8</url>
+<name>Hbo</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2712.m3u8</url>
+<name>Showtime</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2678.m3u8</url>
+<name>Music Choice Alt</name><url>https://edge.music-choice-vc-chainb1.top.comcast.net/VideoChannels/122/media-udyn54561_3208.m3u8</url>
+<name>Starz</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2672.m3u8</url>
+<name>USA</name><url>http://blackice.eddiebo.tk:8000/live/UaWc4SlH3t/12345678/2723.m3u8</url>
